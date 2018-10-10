4e834951aa4479299e05a42de333e6fda8d31134
ivettem84 <cynthiaivette84@gmail.com>
2018-10-09 17:10:57 -0700

    codigo funcionando

--- Fun3.m
+++ Fun3.m
@@ -1,40 +1,54 @@
-function n = Fun3 (Sol,i,f)
+function n = Fun3 (Sol)
 
-load pacientestrain.dat
-load pacientestarg.dat
 
-p=pacientestrain;
-t=pacientestarg;
+load sisto1.dat
+load tarsis.dat
 
- %net=newff(minmax(x),[Sol(i,2),1],{'tansig','purelin','logsig'},'trainlm');
+% x = sisto1;
+% ta = tarsis;
 
-              if(round(Sol(1,1))==1 || round(Sol(1,1))<10)% 1 modulo y  1 capa
-                  net=newff(minmax(p),[round(Sol(1,2)),1],{'tansig','purelin','logsig'},'trainlm');
-              
-              elseif(round(Sol(1,1))==2 || round(Sol(1,1))<=20 )% 1 modulo y  2 capa
-                  net=newff(minmax(p),[round(Sol(1,2)),round(Sol(1,3)),1],{'tansig','tansig','purelin','purelin','logsig'},'trainlm');
-              
-              elseif(round(Sol(1,1))==3 || round(Sol(1,1))<=30)% 1 modulo y  3 capa
-                  net=newff(minmax(p),[round(Sol(1,2)),round(Sol(1,3)),round(Sol(1,4)),1],{'tansig','purelin','purelin','purelin','tansig','tansig','logsig'},'trainlm');
+trnsis=sisto1;
+%Buscar numero maximo en los vectores para normalizar
+maxs=max(trnsis);
+ns=max(maxs);
+%Normalizando datos
+x=trnsis/ns;
+ta=tarsis/ns;
+
+
+disp('Iniciando Entrenamiento...');
+          
+          %%%%%%%%%%%%%%%%%%%%%%%%%%%% PRIMER MODULO %%%%%%%%%%%%%%%%%%%%%%%%%%
+        
+          disp('Inicia Modulo 1...');
+%           
+%           if(Chrom(i,1)==1)%Monolitica
+%              display('Red monolitica en construccion')
+%           elseif (Chrom(i,1)==2) % Modular
+              if(Sol(1,1)==1)% 1 modulo y  1 capa
+                  net=newff(minmax(x),[Sol(1,2),1],{'tansig','purelin','logsig'},'trainlm');
               end
               
-net.LW{1,1}=net.LW{1,1}*0.01;       %Pesos de conexiones
-net.b{1}=net.b{1}*0.01;             %Umbrales
-net.trainParam.show = 100;           % El resultado se muestra cada 100 épocas
-net.trainParam.lr = 0.02;           % Tasa de aprendizaje usado en algún gradiente
-net.trainParam.epochs =1000;         % Máximo numero de iteraciones
-net.trainParam.goal = 1e-5;          % Tolerancia de Error, criterio de parada
-net.trainParam.min_grad=1e-11;         %Minimum performance gradient
-%Start training
-net.trainParam.showWindow=0;
-[net,tr1] = train(net,p, t);
+              if(Sol(1,1)==2)% 1 modulo y  2 capa
+                  net=newff(minmax(x),[Sol(1,2),Sol(1,3),1],{'tansig','tansig','purelin','purelin','logsig'},'trainlm');
+              end
+              
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%Simulando datos entrenados
-A1=sim(net,p);
-Riesgo=round(A1*100);
+              net.LW{2,1} = net.LW{2,1}*0.05;
+              net.b{2}=net.b{2}*0.01;
+              net.trainParam.show=NaN;
+              net.trainParam.goal=0.00001;
+              net.trainParam.lr=0.01;
+              net.trainParam.epochs = Sol(1,4);
+              net.trainParam.showWindow=0;
+              [net,tr1]=train(net,x,ta);
+          
+          disp('Fin del entrenamiento Modulo 1');
+          
+   fnob2fpa();
 
-n=perform(net,t,p);
+   %evalua4(i)=errorestga; 
+  n=errorestga;
 
-%save('framin130201.mat')
+
 
--- fpann.m
+++ fpann.m
@@ -1,59 +1,54 @@
-% --------------------------------------------------------------------% 
-% Flower pollenation algorithm (FPA), or flower algorithm             %
-% Programmed by Xin-She Yang @ May 2012                               % 
-% --------------------------------------------------------------------%
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Notes: This demo program contains the very basic components of      %
-% the flower pollination algorithm (FPA), or flower algorithm (FA),   % 
-% for single objective optimization.    It usually works well for     %
-% unconstrained functions only. For functions/problems with           % 
-% limits/bounds and constraints, constraint-handling techniques       %
-% should be implemented to deal with constrained problems properly.   %
-%                                                                     %   
-% Citation details:                                                   % 
-%1)Xin-She Yang, Flower pollination algorithm for global optimization,%
-% Unconventional Computation and Natural Computation,                 %
-% Lecture Notes in Computer Science, Vol. 7445, pp. 240-249 (2012).   %
-%2)X. S. Yang, M. Karamanoglu, X. S. He, Multi-objective flower       %
-% algorithm for optimization, Procedia in Computer Science,           %
-% vol. 18, pp. 861-868 (2013).                                        %
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 function [best,fmin,N_iter]=fpann(para)
+warning off
+for k1=1:30
 % Default parameters
-if nargin<1
-   para=[20 0.8];
-end
+% if nargin<1
+%    para=[50 0.8];
+% end
 
-n=para(1);           % Population size, typically 10 to 25
-p=para(2);           % probabibility switch
+comienza=now;
 
+n=[5 10 15 20 25 30 35 40 45 50 55 60 65 70 75 80 85 90 95 100 105 110 ...
+    115 120 125 130 135 140 145 150];           % Population size, typically 10 to 25
+p=[0.3 0.7 0.9 0.8 0.2 0.5 0.4 0.8 0.9 0.7 0.6 0.4 0.2 0.1 0.8 0.8 0.2 0.3 0.7 0.6 ...
+    0.8 0.3 0.6 0.9 0.1 0.8 0.7 0.9 0.4 0.8];           % probabibility switch
 % Iteration parameters
-N_iter=2000;            % Total number of iterations
+N_iter=[3600 1200 900 720 600 514 450 400 360 328 300 277 257 240 225 211 ...
+    200 190 180 171 164 157 150 144 139 134 129 124 120];            % Total number of iterations
 
-% Dimension of the search variables
-%d=[1 1 1 1;3 30 30 30];
+n=n(k1);
+p=p(k1);
+N_iter=N_iter(k1);
 
-% Lb=0*ones(size(d),'like',d);
-% Ub=1*ones(size(d),'like',d);
+s1=pwd; %Identify current folder
+s2=['\erroresFPA0810-' num2str(k1) '.txt'];
+%$s2='\erroresFPA0810.txt';
+dir = strcat(s1,s2);
+%--crear arhivo para guardar errores
+error1= fopen(dir, 'wt');
 
 d=4;
 
-Lb=0*ones(1,d);
-Ub=30*ones(1,d);
+% Lb=0*ones(1,d);
+% Ub=30*ones(1,d);
+
+Lb=[1 1 1 1];
+Ub=[2 30 30 700];
 
 % Initialize the population/solutions
 for i=1:n
-  Sol(i,:)=Lb+(Ub-Lb).*rand(1,d);
-  Fitness(i)=fnob2(Sol(i,:), i);
+  Sol(i,:)=round(Lb+(Ub-Lb).*rand(1,d));
+  Fitness(i)=Fun3(Sol(i,:));
+  fprintf(error1,['Error:' num2str(Fitness(i)) ' Iteracion:' int2str(i) ' Individuos:' int2str(i) ' Capas: ' int2str(Sol(i,1)) ' Neuronas capa 1: ' int2str(Sol(i,2))  ' Neuronas Capa 2: ' int2str(Sol(i,3)) ' Epocas: ' int2str(Sol(i,4)) '\n']); 
 end
 
 % Find the current best
 [fmin,I]=min(Fitness);
 best=Sol(I,:);
 S=Sol; 
-
+fprintf(error1,['\n----------------------------------------\n\n']);
 % Start the iterations -- Flower Algorithm 
 for t=1:N_iter
         % Loop over all bats/solutions
@@ -85,12 +80,16 @@ for t=1:N_iter
               S(i,:)=simplebounds(S(i,:),Lb,Ub);
           end
           
+           S=round(S);
+           
           % Evaluate new solutions
-           Fnew=fnob2(S(i,:), i);
+           Fnew=Fun3(S(i,:));
           % If fitness improves (better solutions found), update then
             if (Fnew<=Fitness(i))
                 Sol(i,:)=S(i,:);
                 Fitness(i)=Fnew;
+        
+        fprintf(error1,['Error:' num2str(Fitness(i)) ' Iteracion:' int2str(i) ' Individuos:' int2str(i) ' Capas: ' int2str(Sol(i,1)) ' Neuronas capa 1: ' int2str(Sol(i,2))  ' Neuronas Capa 2: ' int2str(Sol(i,3)) ' Epocas: ' int2str(Sol(i,4)) '\n']);
            end
            
           % Update the current global best
@@ -99,6 +98,7 @@ for t=1:N_iter
                 fmin=Fnew   ;
           end
         end
+
         % Display results every 100 iterations
         if round(t/100)==t/100
         best
@@ -115,10 +115,15 @@ end
 % xlabel('Iterations');
 % ylabel('Best');
 
+termina=now;%%%%%%%%% TIEMPO FINAL
+fprintf(error1,['\n----------------------------------------\n\n']);
+fprintf(error1,['Best Solution:' num2str(best) ' fmin=',num2str(fmin) '\n']);
 disp(['Total number of evaluations: ',num2str(N_iter*n)]);
-disp(['Best solution=',num2str(best),'   fmin=',num2str(fmin)]);
+disp(['Best solution=',num2str(best),'   fmin=',num2str(fmin) '   FPA time:', datestr(termina-comienza,'HH:MM:SS')]);
 
-  
+
+
+end 
 
 % Application of simple constraints
 function s=simplebounds(s,Lb,Ub)
@@ -142,7 +147,7 @@ function L=Levy(d)
 beta=3/2;
 sigma=(gamma(1+beta)*sin(pi*beta/2)/(gamma((1+beta)/2)*beta*2^((beta-1)/2)))^(1/beta);
     u=randn(1,d)*sigma;
-    v=randn(1,d);
+    v=randn(1,d); 
     step=u./abs(v).^(1/beta);
 L=0.01*step; 
 
